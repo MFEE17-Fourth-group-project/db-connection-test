46e56d110d8409260a6ee6b6e8a8eab024f6d8ba
TaylorLiu11 <liuayo1111@gmail.com>
2022-06-14 08:36:04 +0000

    First commit

---
+++ db.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+console.log(process.env);
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : process.env.RDS_HOSTNAME,
+  user     : process.env.RDS_USERNAME,
+  password : process.env.RDS_PASSWORD,
+  port     : process.env.RDS_PORT
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+
+  console.log('Connected to database.');
+});
+
+connection.end();
